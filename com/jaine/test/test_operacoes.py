c8e8db8c14c3cd3e97c79369c58c43a76115ff27
Jaine Ingrid Da Silva Martins <jmartins@meeta.com.br>
2020-04-09 19:17:43 -0300

    atualizado

---
+++ com/jaine/test/test_operacoes.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from com.leandro.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+
+    def setUp(self):
+        self.operacoes = Operacoes()
+    
+    def test_soma(self):
+        self.assertEqual(self.operacoes.soma([2, 2]), 4, "Deveria ser 4")
